220d3de22494e35f7708bc965c97ccd691dd8b45
Saman Teymoori <saman@plasmatic.ai>
2024-07-07 18:21:26 +0330

    BR-4 fix top menu

--- src/app/[lng]/(private)/(dashboard)/home/@topMenu/default.tsx
+++ src/app/[lng]/(private)/(dashboard)/home/@topMenu/default.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { FaCalendar, FaSearch, FaWalking } from "react-icons/fa";
+import { FaQuestion, FaCalendar, FaSearch, FaWalking } from "react-icons/fa";
 
 const Default = () => {
   return (
     <>
-      <div className="shadow-xl self-center rounded-lg rounded-l-none flex flex-none  bg-white ">
-        <div className="ml-4   self-center">
+      <div className="shadow-xl mx-8 pr-4 pl-2 self-center rounded-lg w-64 flex flex-none  bg-white ">
+        <div className="ml-4    self-center">
           <Image
             className="bg-transparent  "
             src={"/images/logo.jpg"}
@@ -18,38 +18,42 @@ const Default = () => {
           <h2 className="text-xl italic my-4">Book & Meet</h2>
         </div>
       </div>
-      <div className="col-span-5 grid  ">
+      <div className="flex-auto">
         <div className=" h-full flex self-center">
           <div className="self-center mx-auto flex-auto">
             <div className="flex mx-auto">
-              <div className="h-16 [&>*]:uppercase [&>*]:self-center [&>*]:h-16 [&>*]:py-4 [&>*]:w-44 [&>*]:text-center rounded-lg [&>*]:mx-auto [&>*]:px-8 ml-8 [&>*]:border  shadow-xl rounded-lg bg-white flex">
+              <div className="h-16 [&>*]:uppercase  [&>*]:self-center [&>*]:h-16 [&>*]:py-4 [&>*]:w-44 [&>*]:text-center rounded-lg [&>*]:mx-auto [&>*]:px-8 ml-8 [&>*]:border  shadow-xl rounded-lg bg-white flex">
                 <div className="rounded-tl-lg flex [&>*]:self-center gap-4 rounded-bl-lg flex">
                   <div>
                     <FaSearch />
                   </div>
-                  <div>Find</div>
+                  <div className="ml-4">Find</div>
                 </div>
-                <div className="rounded-tl-lg flex [&>*]:self-center gap-4 rounded-bl-lg flex">
+                <div className="flex [&>*]:self-center gap-4 flex">
                   <div>
                     <FaCalendar />
                   </div>
-                  <div> Book</div>
+                  <div className="ml-4"> Book</div>
                 </div>
                 <div className="rounded-tr-lg flex [&>*]:self-center gap-4 rounded-br-lg flex">
                   <div>
                     <FaWalking />
                   </div>
-                  <div> Meet</div>
+                  <div className="ml-4"> Meet</div>
                 </div>
               </div>
             </div>
           </div>
           <div className="self-center   flex mx-8 p-4 gap-4">
+            {/* <div className="h-12 w-12 rounded-full bg-green-100"></div>
             <div className="h-12 w-12 rounded-full bg-green-100"></div>
             <div className="h-12 w-12 rounded-full bg-green-100"></div>
-            <div className="h-12 w-12 rounded-full bg-green-100"></div>
-            <div className="h-12 w-12 rounded-full bg-green-100"></div>
-            <div className="h-12 w-12 rounded-full bg-green-100"></div>
+            <div className="h-12 w-12 rounded-full bg-green-100"></div> */}
+            <div className="h-12 w-12 shadow-lg rounded-full flex cursor-pointer bg-green-100">
+              <div className="self-center mx-auto">
+                <FaQuestion />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/app/[lng]/(private)/(dashboard)/home/layout.tsx
+++ src/app/[lng]/(private)/(dashboard)/home/layout.tsx
@@ -5,10 +5,8 @@ import { FaWalking } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa";
 const Layout = async ({ leftMenu, topMenu, swiper }: any) => {
   return (
-    <div className="grid grid-cols-6  w-full  h-24 bg-slate-500 rounded-t-lg">
-      <div className="col-span-6  grid grid-cols-6 self-top h-24">
-        {topMenu}
-      </div>
+    <div className="grid grid-cols-6  w-full  h-32 bg-stone-200 rounded-t-lg">
+      <div className="col-span-6  flex self-top h-32">{topMenu}</div>
       <div className="col-span-6 grid grid-cols-6 flex-auto h-full">
         <div className="h-full ">{leftMenu}</div>
         <div className="col-span-5 mt-4 h-full self-start">{swiper}</div>
