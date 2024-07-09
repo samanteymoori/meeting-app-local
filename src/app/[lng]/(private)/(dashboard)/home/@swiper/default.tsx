7191e94f725f0df1b5e328c329a098ebb31281b2
Saman Teymoori <saman@plasmatic.ai>
2024-07-09 09:40:43 +0330

    BR-4 add some sample profile picture

--- src/app/[lng]/(private)/(dashboard)/home/@swiper/default.tsx
+++ src/app/[lng]/(private)/(dashboard)/home/@swiper/default.tsx
@@ -1,6 +1,14 @@
 "use client";
+import Image from "next/image";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import RoundedImage from "./RoundedImage";
 const Default = () => {
-  return <div></div>;
+  return (
+    <div className="flex">
+      <RoundedImage src={"/images/profile.jpg"} size={"medium"} />
+      <RoundedImage src={"/images/profile2.jpg"} size={"small"} />
+      <RoundedImage src={"/images/profile3.jpg"} size={"small"} />
+    </div>
+  );
 };
 export default Default;
