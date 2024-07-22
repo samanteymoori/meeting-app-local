abfcc0fb1065195d3842779be49ab3029729bea2
Saman Teymoori <saman@plasmatic.ai>
2024-07-22 20:21:05 +0330

    BR-4 add some mock for locations

--- src/mocks/place-list.ts
+++ src/mocks/place-list.ts
@@ -5,6 +5,9 @@ export const placesList: Array<PlaceType> = [
     id: 1,
     name: "Starbox Coffee",
     location: [49.2609, -123.1139],
+    address: " 555 West Georgia Street, Vancouver, BC V6B 1Z5",
+    phone: "(604) 555-5678",
+    workingHours: { start: "10:00", end: "19:00" },
     image: {
       src: "/images/starbucks.jpg",
       size: "rectangle-small",
@@ -13,7 +16,10 @@ export const placesList: Array<PlaceType> = [
   {
     id: 2,
     location: [49.3043, -123.1443],
+    workingHours: { start: "10:00", end: "19:00" },
     name: "Burger King",
+    address: "2705 West Broadway, Vancouver, BC V6K 2G4",
+    phone: "(604) 555-5678",
     image: {
       src: "/images/burger-king.png",
       size: "rectangle-small",
@@ -23,6 +29,9 @@ export const placesList: Array<PlaceType> = [
     id: 3,
     name: "Mac Donalds",
     location: [49.2713, -123.134],
+    workingHours: { start: "10:00", end: "19:00" },
+    address: "333 East Broadway, Vancouver, BC V5T 1W5",
+    phone: "(604) 555-9012",
     image: {
       src: "/images/mc_donalds.png",
       size: "rectangle-small",
@@ -32,6 +41,9 @@ export const placesList: Array<PlaceType> = [
     id: 4,
     name: "KFC",
     location: [49.2745, -123.153],
+    workingHours: { start: "10:00", end: "19:00" },
+    address: "1045 Denman Street, Vancouver, BC V6G 2M4",
+    phone: "(604) 555-3456",
     image: {
       src: "/images/kfc.png",
       size: "rectangle-small",
--- src/types/PlaceType.ts
+++ src/types/PlaceType.ts
@@ -2,6 +2,12 @@ import { LatLngExpression, LatLngTuple } from "leaflet";
 
 export type PlaceType = {
   location: LatLngExpression | LatLngTuple;
+  workingHours: {
+    start: string;
+    end: string;
+  };
+  address: string;
+  phone: string;
   name: string;
   image: {
     src: string;
