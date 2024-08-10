cd4c91c9f64d7fc4b89689fef2e929c6fdeeded9
Saman Teymoori <saman@plasmatic.ai>
2024-08-10 15:51:22 +0330

    BR-4 user authentication

--- src/app/api/auth/user/login/route.ts
+++ src/app/api/auth/user/login/route.ts
@@ -2,41 +2,35 @@ import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-const SECRET_KEY = "your-secret-key"; // Use a secure and private key
+const SECRET_KEY =
+  "J$wX9ZPq3u8sT^C@Dg6M2k!rF5v*QbPj7yG^T4mNtL&bS@q3#Pp9%wZzM5fUeRn";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const requestBody = await req.json();
+  const { username, password } = requestBody;
   console.log({ requestBody });
   // Authenticate user
-  // You should replace this with real authentication logic
-  // if (
-  //   requestBody.username === "saman.teymoori@hotmail.com" &&
-  //   requestBody.password === "Test123!"
-  // ) {
-  // Create JWT token
-  const token = jwt.sign(
-    {
-      username: requestBody.username,
-    },
-    SECRET_KEY
-  );
-  cookies().set({
-    name: "jwt_token",
-    value: JSON.stringify(token),
-    httpOnly: true,
-    path: "/",
-    secure: true,
-  });
+  if (username === "saman.teymoori@hotmail.com" && password === "Test123!") {
+    // Create JWT token
+    const token = jwt.sign(
+      {
+        username: requestBody.username,
+      },
+      SECRET_KEY
+    );
+    cookies().set({
+      name: "jwt_token",
+      value: JSON.stringify(token),
+      httpOnly: true,
+      path: "/",
+      secure: true,
+    });
 
-  return NextResponse.json({ token }, { status: 200 });
-
-  // // Set cookie with token
-
-  // return NextResponse.json({ message: "Login successful" }, 200);
-  // } else {
-  //   return NextResponse.json(
-  //     { error: "Internal Server Error2 " },
-  //     { status: 500 }
-  //   );
-  // }
+    return NextResponse.json({ token }, { status: 200 });
+  } else {
+    return NextResponse.json(
+      { error: "Invalid username or password" },
+      { status: 403 }
+    );
+  }
 }
