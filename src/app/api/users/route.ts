5ea2447b2092befa87062a17c90465e2173f3f1d
Saman Teymoori <saman@plasmatic.ai>
2024-08-13 13:13:19 +0330

    BR-4  add connection string to the env file

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,15 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "",
-  port: 5432,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  database: process.env.NEXT_PUBLIC_DB_NAME,
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+  port: process.env.NEXT_PUBLIC_DB_PORT,
 });
 export async function GET(request: NextRequest) {
   try {
-    const res = await pool.query("SELECT * FROM users");
+    const res = await pool.query("SELECT * FROM users ORDER BY first_name");
 
     return NextResponse.json({ rows: res.rows }, { status: 200 });
   } catch (error) {
