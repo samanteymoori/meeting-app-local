91659e6419c1c16c4c927e4ac27056b07848b9ae
Saman Teymoori <saman@plasmatic.ai>
2024-08-13 14:14:23 +0330

    BR-4 get user by id

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -6,8 +6,10 @@ export async function GET(request: NextRequest) {
   const client = await pool.connect();
 
   try {
+    const values = ["87e88d18-1af1-42cc-bcf1-7050e56b9b65"];
     const res = await client.query(
-      "SELECT * FROM users WHERE id=@id ORDER BY first_name"
+      "SELECT * FROM users WHERE id=$1 ORDER BY first_name",
+      values
     );
     return NextResponse.json({ rows: res.rows }, { status: 200 });
   } catch (error) {
