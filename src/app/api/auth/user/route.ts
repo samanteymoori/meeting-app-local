39ce377325f38a9ff11f56818ec9892bb417931d
Saman Teymoori <saman@plasmatic.ai>
2024-08-16 10:25:34 +0330

    BR-4 verify jwt token API

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,7 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+const SECRET_KEY =
+  "J$wX9ZPq3u8sT^C@Dg6M2k!rF5v*QbPj7yG^T4mNtL&bS@q3#Pp9%wZzM5fUeRn";
 
 export async function GET(request: NextRequest) {
-  const jwt_token = request.cookies.get("jwt_token");
+  try {
+    const jwt_token = request.cookies.get("jwt_token");
+    const tokenToVerify = jwt_token?.value.replaceAll(`\"`, ``) as any;
+    const verified = jwt.verify(tokenToVerify, SECRET_KEY);
 
-  return NextResponse.json({ jwt_token });
+    return NextResponse.json({ id: (verified as any)?.id }, { status: 200 });
+  } catch {
+    return NextResponse.json(
+      { error: "Unauthorized access." },
+      { status: 403 }
+    );
+  }
 }
