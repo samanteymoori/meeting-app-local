fdcc82210d5f43edbae59bb8d71a2c519b353c03
Saman Teymoori <saman@plasmatic.ai>
2025-09-21 14:07:21 -0700

    fix login and forgot password

--- src/app/[lng]/(public)/login/email/layout.tsx
+++ src/app/[lng]/(public)/login/email/layout.tsx
@@ -3,9 +3,15 @@ import React from "react";
 const Layout = async ({ children, form }: any) => {
   return (
     <div className="h-screen flex">
-      <img src={"/images/logo.png"} width={"w-1/2"} />
-      {children}
-      {form}
+      <img
+        className="hidden lg:block"
+        src={"/images/logo.png"}
+        width={"w-1/2"}
+      />
+      <div className="grid">
+        {children}
+        {form}
+      </div>
     </div>
   );
 };
--- src/app/[lng]/(public)/login/forgetpassword/layout.tsx
+++ src/app/[lng]/(public)/login/forgetpassword/layout.tsx
@@ -3,7 +3,14 @@ import React from "react";
 const Layout = async ({ children, form }: any) => {
   return (
     <div className="h-screen flex">
-      <img src={"/images/logo.png"} width={"w-1/2"} />
+      <img
+        className="hidden lg:block"
+        src={"/images/logo.png"}
+        width={"w-1/2"}
+      />
+      <div>
+        <h1 className="text-black">Find, Book, Meet</h1>
+      </div>
       {children}
       {form}
     </div>
