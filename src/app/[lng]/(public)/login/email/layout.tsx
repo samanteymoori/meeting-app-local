d6aa5369c867bc5f59feb7386129af8ac527afc9
Saman Teymoori <saman@plasmatic.ai>
2025-10-04 13:29:38 -0700

    fix layout

--- src/app/[lng]/(public)/login/email/layout.tsx
+++ src/app/[lng]/(public)/login/email/layout.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Layout = async ({ children, form }: any) => {
   return (
-    <div className="h-screen w-screen grid grid-cols-3">
-      <div className="col-span-2">
+    <div className="h-screen w-screen md:grid md:grid-cols-3">
+      <div className="md:col-span-2">
         <img
           className="hidden cover h-screen lg:block"
           src={"/images/logo.png"}
           width={"w-1/2"}
         />
       </div>
-      <div className="grid">
+      <div className="grid h-screen">
         {children}
         {form}
       </div>
